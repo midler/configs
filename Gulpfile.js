6a53b0c197deb8ae3e4a514df9cad21953ddf534
Midler <midler@users.noreply.github.com>
2015-04-29 14:29:57 +0300

    Fully rewrite

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,193 +1,97 @@
-'use strict';
-var gulp = require('gulp');
-// var svgmin = require('gulp-svgmin');
-var imgmin = require('gulp-imagemin');
-// var hb = require('gulp-hb');
-var less = require('gulp-less');
-var pleeease = require('gulp-pleeease');
-var rename = require('gulp-rename');
-var plumber = require('gulp-plumber');
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var runSequence = require('run-sequence');
+var imagemin = require('gulp-imagemin');
+var prefix = require('gulp-autoprefixer');
 var stylus = require('gulp-stylus');
-var flatten = require('gulp-flatten');
-var svgconvert = require('gulp-svg2png');
-var uglify = require('gulp-uglify');
+var rimraf = require('rimraf');
+var csscomb = require('gulp-csscomb');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var cmq = require('gulp-combine-media-queries');
+var nmq = require('gulp-no-media-queries');
+var minifyCss = require('gulp-minify-css');
 
 
-/*===================Paths===================== */
-//base path
-var bpath = {
-  src: './src/',
-  dest: './dest/'   
-};
-
-var path = {
-  images: {
-    src: bpath.src + 'images/',
-    dest: bpath.dest + 'images/',
-    srcopt: bpath.src + 'images/opt/'
-  },
-  scripts: {
-    src: bpath.src + 'js/',
-    dest: bpath.dest + 'js/',
-  },
-  styles: {
-    src: bpath.src + 'stylus/',
-    dest: bpath.dest + 'css/'
-  },
-  html: {
-    src: bpath.src + 'html/',
-    dest: bpath.dest
-  },
-  fonts: {
-    src: bpath.src + 'fonts',
-    dest: bpath.dest + 'fonts/'
-  }
-
-};
-
-var pathImagesFormat = {
-  jpeg: path.images.src + 'jpeg',
-  png: path.images.src + 'png',
-  svg: path.images.src + 'svg'
-};
-
-
-/*===================Plug-In===================== */
-
-
-// gulp.task('svgmin', function () {
-//   return gulp.src(pathImagesFormat.svg)
-//     .pipe(plumber())
-//     .pipe(svgmin())
-//     .pipe(gulp.dest(bpath.dest + 'images/'));
-// });
-
-gulp.task('imgmin', ['svgconvert'],
-  function () {
-    return gulp.src(path.images.src + '/**/*.{jpg,jpeg,png,gif}')
-      .pipe(plumber())
-      .pipe(imgmin({
-        progressive: true,
-        interlaced: true,
-        optimizationLevel: 3
-      }))
-      .pipe(rename({
-        prefix: '2014_Report_'
-      }))
-      .pipe(flatten())
-      .pipe(gulp.dest(path.images.dest));
-  });
-
-
-gulp.task('svgconvert', function () {
-  console.log(pathImagesFormat.svg + '/*.svg');
-  return gulp.src(pathImagesFormat.svg + '/*.svg')
-    .pipe(plumber())
-    .pipe(svgconvert())
-    .pipe(gulp.dest(pathImagesFormat.png));
-});
-
-// gulp.task('hbs', function () {
-//   return gulp
-//     .src(path.hbs.src + '/*')
-//     .pipe(plumber())
-//     .pipe(hb())
-//     .pipe(gulp.dest(bpath.dest));
-// });
-
-gulp.task('move-html', function () {
-  return gulp.src(path.html.src + '/**/*.html')
-    .pipe(plumber())
-    .pipe(gulp.dest(path.html.dest))
-    .pipe(reload({
-      stream: true
-    }));
-});
-
-gulp.task('move-fonts', function () {
-  return gulp.src(path.fonts.src + '/**/*.*')
-    .pipe(plumber())
-    .pipe(gulp.dest(path.fonts.dest));
-});
-
-gulp.task('move-js', function () {
-  return gulp.src(path.scripts.src + '/**/*.*')
-    .pipe(plumber())
-    .pipe(uglify({
-      compress: true
-    }))
-    .pipe(rename({
-      suffix: '.min',
-      extname: '.js'
-    }))
-    .pipe(gulp.dest(path.scripts.dest));
-});
-
-gulp.task('less', function () {
-  return gulp
-    .src(path.styles.src + '**/*.less')
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(pleeease())
-    // .pipe(rename({
-    //   suffix: '.min',
-    //   extname: '.css'
-    // }))
-    .pipe(gulp.dest(bpath.dest + '/css'));
+gulp.task('clean', function (cb) {
+  return rimraf('./dist', cb);
 });
 
 
-gulp.task('stylus', function () {
-  return gulp
-    .src(path.styles.src + '/*.styl')
+gulp.task('styles', function () {
+  return gulp.src('src/stylus/**/*.styl')
     .pipe(plumber())
     .pipe(stylus({
       "include css": true
     }))
-    .pipe(pleeease({
-      "autoprefixer": {
-        "browsers": ["last 4 versions", "ie 8"]
-      },
-      "filters": true,
-      "rem": true,
-      "pseudoElements": true,
-      "opacity": true,
-      "import": true,
-      "minifier": true,
-      "mqpacker": true
+    .pipe(prefix('last 4 version, > 1%, ie 8, ie 9'))
+    .pipe(cmq({
+      log: true
     }))
-    .pipe(gulp.dest(bpath.dest + '/css/min'))
+    .pipe(csscomb())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(nmq())
+    .pipe(rename({
+      suffix: '.ie',
+      extname: '.css'
+    }))
+    .pipe(gulp.dest('dist/css/ie/'))
     .pipe(reload({
       stream: true
     }));
 });
 
-gulp.task('please', function () {
-  return gulp
-    .src(path.styles.dest + '/*.css')
+gulp.task('styles:build', function () {
+  return gulp.src('src/stylus/**/*.styl')
     .pipe(plumber())
-    .pipe(pleeease({
-      "autoprefixer": {
-        "browsers": ["last 4 versions", "ie 8"]
-      },
-      "filters": true,
-      "rem": true,
-      "pseudoElements": true,
-      "opacity": true,
-      "import": true,
-      "minifier": true,
-      "mqpacker": true
+    .pipe(stylus({
+      "include css": true
     }))
-    .pipe(gulp.dest(bpath.dest + '/css/min/'));
+    .pipe(prefix('last 4 version, > 1%, ie 8, ie 9'))
+    .pipe(cmq())
+    .pipe(csscomb())
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(nmq())
+    .pipe(rename({
+      suffix: '.ie',
+      extname: '.css'
+    }))
+    .pipe(gulp.dest('dist/css/ie/'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
-gulp.task('browser-sync', function () {
+gulp.task('images', function () {
+  return gulp.src('src/images/*.*')
+    .pipe(imagemin({
+      progressive: true,
+      optimizationLevel: 4,
+      multipass: true
+    }))
+    .pipe(rename({
+      prefix: '2015-04-endpoint_'
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('server', function () {
   browserSync({
     server: {
-      baseDir: bpath.dest
+      baseDir: 'dist/'
     },
     logLevel: "info",
     open: false,
@@ -196,27 +100,23 @@ gulp.task('browser-sync', function () {
     //     location: true,
     //     forms: true,
     //     scroll: false
-    // }
+    // } 
     ghostMode: false,
     notify: false,
+    injectChanges: true,
+    codeSync: true,
     reloadDelay: 500
   });
+
 });
 
 
-
-/* ===================== Main Tasks ========================== */
-
-gulp.task('default', function () {
-  console.log('Mida\'s building system');
+gulp.task('build', function (callback) {
+  runSequence(['images'], ['styles:build'], callback);
 });
 
-
-gulp.task('server', ['imgmin', 'move-js', 'move-fonts', 'move-html', 'stylus', 'browser-sync'], function () {
-  gulp.watch(path.styles.src + '/**/*.styl', ['stylus']);
-  gulp.watch(path.html.src + "/*.html", ['move-html']);
+gulp.task('default', function (callback) {
+  runSequence('clean', ['images'], ['styles', 'html', 'server'], callback);
+  gulp.watch('src/stylus/**/*.styl', ['styles']);
+  gulp.watch('src/*.html', ['html']);
 });
-
-
-
-// gulp.task('server-nob', ['browser-sync']);
